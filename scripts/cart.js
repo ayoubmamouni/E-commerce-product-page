d132806c512d2c2721838b40f7140c764c1667fd
Ayoub Mamouni <ayoub.mamouni@yandex.com>
2022-05-02 20:00:33 +0000

    add javascript code

---
+++ scripts/cart.js
@@ -0,0 +1,57 @@
+export const cart = () => {
+  const minus = document.querySelector("#minus");
+  let counter = document.querySelector("#counter");
+  const plus = document.querySelector("#plus");
+
+  const numberOfqtty = document.querySelector("#numberOfqtty");
+  const total = document.querySelector(".total");
+  const checkoutBtn = document.querySelector("#checkoutBtn");
+
+  const cartItems = document.querySelector(".cartItems");
+  const emptyTxt = document.querySelector(".empty-txt");
+  const trash = document.querySelector(".trash");
+
+  const addToCart = document.querySelector(".add-to-cart");
+  const cartIcon = document.querySelector(".cart-icon");
+  const cartCard = document.querySelector(".cart-card");
+  const notificationCartNumber = document.querySelector(
+    ".notificationCartNumber"
+  );
+
+  cartIcon.addEventListener("click", () => {
+    cartCard.classList.toggle("showCart");
+  });
+
+  let price = 125.0;
+  let c = 0;
+
+  plus.addEventListener("click", () => {
+    c++;
+    counter.innerText = c;
+  });
+  minus.addEventListener("click", () => {
+    c > 0 && c--;
+    counter.innerText = c;
+  });
+
+  addToCart.addEventListener("click", () => {
+    let totalPrice = price * c;
+    if (c == 0) return console.log("Your cart is empty");
+    notificationCartNumber.innerText = c;
+    notificationCartNumber.classList.add("show");
+    emptyTxt.classList.add("hide");
+    cartItems.classList.add("show");
+    checkoutBtn.classList.add("show");
+    numberOfqtty.innerText = c;
+    total.innerText = `$${totalPrice}.00`;
+    cartCard.classList.add("showCart");
+  });
+  trash.addEventListener("click", () => {
+    c = 0;
+    counter.innerText = 0;
+    notificationCartNumber.classList.remove("show");
+    emptyTxt.classList.remove("hide");
+    cartItems.classList.remove("show");
+    checkoutBtn.classList.remove("show");
+  });
+};
---
+++ scripts/creatElements.js
@@ -0,0 +1,43 @@
+export async function GetComponent(element, fileName) {
+  try {
+    let response = await fetch(`/components/${fileName}.html`);
+    const htmlFile = await response.text();
+    element.innerHTML = htmlFile;
+  } catch (err) {
+    console.log("Fetch error: " + err);
+  }
+}
+
+function createElement(element, fileName, className, elementId) {
+  const htmlElement = document.createElement(element);
+  className && htmlElement.classList.add(className);
+  if (elementId) htmlElement.id = elementId;
+  document.getElementById("app").append(htmlElement);
+  if (className) {
+    GetComponent(document.querySelector(`.${className}`), fileName);
+  } else {
+    GetComponent(document.querySelector(htmlElement), fileName);
+  }
+}
+
+//Create a nav
+const navElement = {
+  elementName: "nav",
+  fileName: "nav",
+  className: "nav",
+  elementId: "nav",
+};
+createElement(
+  navElement.elementName,
+  navElement.fileName,
+  navElement.className,
+  navElement.elementId
+);
+//create a main
+const main = {
+  elementName: "main",
+  fileName: "mainContent",
+  className: "main",
+  elementId: "",
+};
+createElement(main.elementName, main.fileName, main.className, main.elementId);
